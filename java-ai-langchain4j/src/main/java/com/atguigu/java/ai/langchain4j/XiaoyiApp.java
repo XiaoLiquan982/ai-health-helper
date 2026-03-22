a71364cb86d8a8c4e981ce97c96ce22518a76fc2
xiao <xlq2494838259@163.com>
2026-03-22 18:31:43 +0800

    first commit

---
+++ java-ai-langchain4j/src/main/java/com/atguigu/java/ai/langchain4j/XiaoyiApp.java
@@ -0,0 +1,11 @@
+package com.atguigu.java.ai.langchain4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaoyiApp {
+    public static void main(String[] args) {
+        SpringApplication.run(XiaoyiApp.class, args);
+    }
+}
